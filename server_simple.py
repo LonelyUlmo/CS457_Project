5ef273d28c127de1eac3e60696b34a72cf558b88
Jonathan Evans <u1098099@colostate.edu>
2024-11-13 20:28:06 -0700

    This code aims to allow an echo client/server to handle multiple messages from the client.

---
+++ server_simple.py
@@ -0,0 +1,63 @@
+import sys
+import socket
+import selectors
+import types
+
+sel = selectors.DefaultSelector()
+
+def accept_wrapper(sock):
+    conn, addr = sock.accept()  # Should be ready to read
+    print("accepted connection from", addr)
+    conn.setblocking(False)
+    data = types.SimpleNamespace(addr=addr, inb=b"", outb=b"")
+    events = selectors.EVENT_READ | selectors.EVENT_WRITE
+    sel.register(conn, events, data=data)
+
+def service_connection(key, mask):
+    sock = key.fileobj
+    data = key.data
+    if mask & selectors.EVENT_READ:
+        recv_data = sock.recv(4096)
+        if recv_data:
+            print(f"received \"{recv_data.decode()}\" from {data.addr}")
+            # data.outb += recv_data
+            data.outb = recv_data
+        else:
+            close_connection(sock, data)
+    if mask & selectors.EVENT_WRITE:
+        if data.outb:
+            print("echoing", repr(data.outb), "to", data.addr)
+            sent = sock.send(data.outb)  # Should be ready to write
+            data.outb = data.outb[sent:]
+
+def close_connection(sock, data):
+    print("closing connection to", data.addr)
+    sel.unregister(sock)
+    sock.close()
+
+
+# main program
+host = '0.0.0.0'  # listens on 0.0.0.0 for the real internet
+port = 12358
+
+# set up the listening socket and register it with the SELECT mechanism
+lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+lsock.bind((host, port))
+lsock.listen()
+print("listening on", (host, port))
+lsock.setblocking(False)
+sel.register(lsock, selectors.EVENT_READ, data=None)
+
+# the main event loop
+try:
+    while True:
+        events = sel.select(timeout=None)
+        for key, mask in events:
+            if key.data is None:
+                accept_wrapper(key.fileobj)
+            else:
+                service_connection(key, mask)
+except KeyboardInterrupt:
+    print("caught keyboard interrupt, exiting")
+finally:
+    sel.close()
