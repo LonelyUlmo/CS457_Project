ffa939cd2569eab93e9547c5ea8cd7c125c62735
Jonathan Evans <u1098099@colostate.edu>
2024-11-14 21:19:23 -0700

    Simplified the server and client to the bare bones.

--- client_simple.py
+++ client_simple.py
@@ -5,68 +5,31 @@ import types
 
 sel = selectors.DefaultSelector()
 
-def start_connections(host, port):
-    server_addr = (host, port)
-    print("starting connection to", server_addr)
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.setblocking(False)
-    sock.connect_ex(server_addr)
-    events = selectors.EVENT_READ | selectors.EVENT_WRITE
-    data = types.SimpleNamespace(
-        inb=b"",
-        outb=b"")
-    sel.register(sock, events, data=data)
-
-def service_connection(key, mask):
-    sock = key.fileobj
-    data = key.data
-    if mask & selectors.EVENT_READ:
-        recv_data = sock.recv(4096)
-        if recv_data:
-            print("received", repr(recv_data))
-        else:
-            close_connection(sock)
-    if mask & selectors.EVENT_WRITE:
-        send_message(sock, data)
-
-def close_connection(sock):
-    print("closing connection")
-    sel.unregister(sock)
-    sock.close()
-
-def send_message(sock, data):
-    message = input("Enter Message Here: ")
-    print("Current value of data.outb is:", data.outb)
-    # Exit condition
-    if message == "exit":
-        close_connection(sock)
-        return
-    # Send the message
-    if data.outb == message: # reset outb value if the whole message has been sent
-        data.outb = b""
-    if not data.outb:
-        data.outb = message.encode("utf-8")
-    if data.outb:
-        print("sending", repr(data.outb))
-        sent = sock.send(data.outb)  # Should be ready to write
-        data.outb = data.outb[sent:]
-
 # main program
 host = '129.82.45.121' # this is the IP address of the blowfish machine (I hope this doesn't change periodically. Idk how to find it other than running the 'hostname -I' command on the host machine)
 port = 12358
-sock = start_connections(host, port)
 
-# the event loop
-try:
-    while True:
-        events = sel.select(timeout=1)
-        if events:
-            for key, mask in events:
-                service_connection(key, mask)
-        # Check for a socket being monitored to continue.
-        if not sel.get_map():
-            break
-except KeyboardInterrupt:
-    print("caught keyboard interrupt, exiting")
-finally:
-    sel.close()
+# Create a client socket
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Connect to the server
+client_socket.connect((host, port))
+
+while 1:
+    # Get user input
+    message = input("Enter a message: ")
+
+    # Send the input to the server
+    client_socket.send(message.encode())
+
+    # Receive the server's response
+    server_response = client_socket.recv(1024)
+
+    # Print the server's response
+    print(f"Server responded with: {server_response.decode()}")
+
+    # Exit condition
+    if message == "exit":
+        break
+
+# Close the client socket
+client_socket.close()
--- server_simple.py
+++ server_simple.py
@@ -6,58 +6,54 @@ import types
 sel = selectors.DefaultSelector()
 
 def accept_wrapper(sock):
-    conn, addr = sock.accept()  # Should be ready to read
-    print("accepted connection from", addr)
+    conn, addr = sock.accept()  # Accept the connection
+    print("Accepted connection from", addr)
     conn.setblocking(False)
-    data = types.SimpleNamespace(addr=addr, inb=b"", outb=b"")
-    events = selectors.EVENT_READ | selectors.EVENT_WRITE
-    sel.register(conn, events, data=data)
+    data = types.SimpleNamespace(addr=addr, outb=b"")
+    sel.register(conn, selectors.EVENT_READ | selectors.EVENT_WRITE, data=data)
 
 def service_connection(key, mask):
     sock = key.fileobj
     data = key.data
     if mask & selectors.EVENT_READ:
-        recv_data = sock.recv(4096)
+        recv_data = sock.recv(4096)  # Receive data
         if recv_data:
-            print(f"received \"{recv_data.decode()}\" from {data.addr}")
-            # data.outb += recv_data
-            data.outb = recv_data
+            print(f"Received \"{recv_data.decode()}\" from {data.addr}")
+            data.outb = recv_data  # Prepare to echo data back
         else:
             close_connection(sock, data)
-    if mask & selectors.EVENT_WRITE:
-        if data.outb:
-            print("echoing", repr(data.outb), "to", data.addr)
-            sent = sock.send(data.outb)  # Should be ready to write
-            data.outb = data.outb[sent:]
+
+    if mask & selectors.EVENT_WRITE and data.outb:
+        print("Echoing", repr(data.outb), "to", data.addr)
+        sent = sock.send(data.outb)  # Echo data
+        data.outb = data.outb[sent:]  # Clear sent data
 
 def close_connection(sock, data):
-    print("closing connection to", data.addr)
+    print("Closing connection to", data.addr)
     sel.unregister(sock)
     sock.close()
 
-
-# main program
-host = '0.0.0.0'  # listens on 0.0.0.0 for the real internet
+# Main program setup
+host = '0.0.0.0'  # Listen on all network interfaces
 port = 12358
 
-# set up the listening socket and register it with the SELECT mechanism
+# Set up the listening socket and register it with the selector
 lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 lsock.bind((host, port))
 lsock.listen()
-print("listening on", (host, port))
+print("Listening on", (host, port))
 lsock.setblocking(False)
 sel.register(lsock, selectors.EVENT_READ, data=None)
 
-# the main event loop
+# Main event loop
 try:
     while True:
-        events = sel.select(timeout=None)
-        for key, mask in events:
+        for key, mask in sel.select(timeout=None):
             if key.data is None:
                 accept_wrapper(key.fileobj)
             else:
                 service_connection(key, mask)
 except KeyboardInterrupt:
-    print("caught keyboard interrupt, exiting")
+    print("Caught keyboard interrupt, exiting")
 finally:
     sel.close()
